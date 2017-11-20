e92cd321200748fd3d3085507ef1d26abae82bdc
Kael Zhang <i+github@kael.me>
2017-11-21 00:40:39 +0800

    fixes rollup with commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   input: 'src/index.js',
@@ -9,6 +10,9 @@ export default {
   },
   plugins: [
     resolve(),
+    commonjs({
+      include: ['node_modules/**']
+    }),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     })
