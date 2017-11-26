599da7cd86d89eb38320c25f7168e393089fe457
Kael Zhang <i+github@kael.me>
2017-11-26 11:15:13 +0800

    2.0.1: avoid bundle external modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
+import pkg from './package.json'
 
 export default {
   input: 'src/index.js',
@@ -8,6 +9,7 @@ export default {
     file: 'lib/index.js',
     format: 'cjs'
   },
+  external: Object.keys(pkg.dependencies),
   plugins: [
     resolve(),
     commonjs({
